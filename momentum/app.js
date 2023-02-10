964419c919e3f1c5cdac2843df05b49fc10e8a13
이시흔 <dudwo5600@naver.com>
2023-02-10 06:08:22 +0000

    js

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,19 @@
-alert("hi");
+const calculator={
+	add: function(a,b){
+		console.log(a+b);
+	},
+	minus: function(a,b){
+		console.log(a-b);
+	},
+	divide: function(a,b){
+		console.log(a/b);
+	},
+	powerOf: function(a,b){
+		console.log(a**b);
+	},
+};
+
+calculator.add(2,4);
+calculator.minus(2,4);
+calculator.divide(2,4);
+calculator.powerOf(2,4);
