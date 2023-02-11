ec45ce426a78bdc1266e7949b93fad7027b94be3
이시흔 <dudwo5600@naver.com>
2023-02-11 12:32:53 +0000

    js

--- momentum/app.js
+++ momentum/app.js
@@ -1,19 +1,13 @@
-const calculator={
-	add: function(a,b){
-		console.log(a+b);
-	},
-	minus: function(a,b){
-		console.log(a-b);
-	},
-	divide: function(a,b){
-		console.log(a/b);
-	},
-	powerOf: function(a,b){
-		console.log(a**b);
-	},
-};
+const age = parseInt(prompt("How old are you?"));
 
-calculator.add(2,4);
-calculator.minus(2,4);
-calculator.divide(2,4);
-calculator.powerOf(2,4);
+if (isNaN(age) || age<0){
+	console.log("Please write a real positive number");
+} else if (age<18){
+	console.log("You are too young");
+}else if (age>=18 && age <=50){
+	console.log("You can drink");
+}else if (age>50 && age<=80){
+	console.log("You should exercise");
+}else if (age>80){
+	console.log("You can do whatever you want");
+}
