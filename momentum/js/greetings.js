33bf738c35aa3d385a36f1c131d9d5e96fd5fead
이시흔 <dudwo5600@naver.com>
2023-02-18 04:20:18 +0000

    js

--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const logoutForm= document.querySelector("#logout-form");
+
 
 const HIDDEN_CLASSNAME="hidden";
 const USERNAME_KEY="username";
@@ -13,9 +15,22 @@ function onLoginSubmit(event){
 	paintGreetings(username);
 }
 
+
+function onLogoutSubmit(){
+	// 로그아웃 시 localStorage 내의 데이터 지우기
+	localStorage.removeItem(USERNAME_KEY);
+}
+
 function paintGreetings(username){
 	greeting.innerText= `Hello ${username}`;
 	greeting.classList.remove(HIDDEN_CLASSNAME);
+	
+	// 로그인 성공 시 로그아웃 가능하게 logout button 제작
+	const logoutButton=document.createElement("button");
+	logoutButton.innerText="logout";
+	logoutForm.classList.remove(HIDDEN_CLASSNAME);
+	logoutForm.appendChild(logoutButton); 
+	logoutForm.addEventListener("click",onLogoutSubmit);
 }
 
 const savedUsername=localStorage.getItem(USERNAME_KEY);
